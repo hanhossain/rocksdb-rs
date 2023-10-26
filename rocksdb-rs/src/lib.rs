f653ae0759e4f3cfb2f609b5c0f81523bcf68fbf
Han Hossain <hanhossain@outlook.com>
2023-10-26 00:45:17 -0700

    impl hello_world in rust

--- rocksdb-rs/src/lib.rs
+++ rocksdb-rs/src/lib.rs
@@ -1,3 +1,14 @@
+#[cxx::bridge]
+mod ffi {
+    extern "Rust" {
+        fn hello_world(caller: &str);
+    }
+}
+
+pub fn hello_world(caller: &str) {
+    println!("Hello world from rust! I was called from {caller}.");
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
