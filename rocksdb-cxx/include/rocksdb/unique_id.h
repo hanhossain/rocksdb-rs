53ba4718dde52865f7e6605b7f0718f8be7248c3
Han Hossain <hanhossain@outlook.com>
2023-12-25 20:57:29 -0600

    include import in unique_id.h

--- rocksdb-cxx/include/rocksdb/unique_id.h
+++ rocksdb-cxx/include/rocksdb/unique_id.h
@@ -7,6 +7,12 @@
 
 #include "rocksdb/table_properties.h"
 
+#ifndef ROCKSDB_RS
+#include "rocksdb-rs-cxx/unique_id.h"
+#else
+#include "rocksdb-rs/src/unique_id.rs.h"
+#endif
+
 namespace rocksdb {
 
 // Computes a stable, universally unique 128-bit (16 binary char) identifier
