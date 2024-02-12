ac0b86c85c744ce432b14bb1a408f3a631be61f3
Han Hossain <hanhossain@outlook.com>
2024-02-12 00:19:55 -0800

    iterate all paths and break on first missing mapping

--- tools/dependency-graph/src/main.rs
+++ tools/dependency-graph/src/main.rs
@@ -1,5 +1,5 @@
 use regex::RegexBuilder;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::path::PathBuf;
 use walkdir::WalkDir;
 
@@ -32,30 +32,36 @@ fn main() -> anyhow::Result<()> {
                 .map_or(false, |e| e == "cc" || e == "c" || e == "h")
         })
         .map(|e| e.into_path())
-        .map(|p| (p, HashMap::new()))
-        .collect::<HashMap<PathBuf, HashMap<String, IncludeMapping>>>();
-
-    let path = paths.keys().next().unwrap().clone();
-    println!("{:?}", path);
-    let content = std::fs::read_to_string(&path)?;
+        .collect::<HashSet<_>>();
 
     let re = RegexBuilder::new("^#include \"(\\S+)\"")
         .multi_line(true)
         .build()?;
 
-    let mut include_mappings = HashMap::new();
-    for (_, [include]) in re.captures_iter(&content).map(|c| c.extract()) {
-        if !include_mappings.contains_key(include) {
-            let path = find_path(&cxx_root, include, &path, &paths);
-            let include_mapping = IncludeMapping {
-                include: include.to_string(),
-                path,
-            };
-            include_mappings.insert(include.to_string(), include_mapping);
+    let mut path_mappings = HashMap::new();
+
+    for path in &paths {
+        let content = std::fs::read_to_string(path)?;
+        let mut include_mappings = HashMap::new();
+        for (_, [include]) in re.captures_iter(&content).map(|c| c.extract()) {
+            if !include_mappings.contains_key(include) {
+                let path = find_path(&cxx_root, include, path, &paths);
+                let include_mapping = IncludeMapping {
+                    include: include.to_string(),
+                    path,
+                };
+                include_mappings.insert(include.to_string(), include_mapping);
+            }
         }
+        path_mappings.insert(path.clone(), include_mappings);
     }
 
-    println!("{:#?}", include_mappings);
+    for (path, mappings) in path_mappings {
+        if mappings.values().any(|m| m.path.is_none()) {
+            println!("{}: {:#?}", path.display(), mappings);
+            break;
+        }
+    }
 
     Ok(())
 }
@@ -64,23 +70,26 @@ fn find_path(
     cxx_root: &PathBuf,
     include: &str,
     current_path: &PathBuf,
-    paths: &HashMap<PathBuf, HashMap<String, IncludeMapping>>,
+    paths: &HashSet<PathBuf>,
 ) -> Option<PathBuf> {
     // find relative to cxx_root
     let needle = cxx_root.join(include);
-    if paths.contains_key(&needle) {
+    if paths.contains(&needle) {
         return Some(needle);
     }
 
     // find relative to include folder
     let needle = cxx_root.join("include").join(include);
-    if paths.contains_key(&needle) {
+    if paths.contains(&needle) {
         return Some(needle);
     }
 
     // find relative to current path
-    let needle = current_path.join(include);
-    if paths.contains_key(&needle) {
+    let needle = current_path
+        .parent()
+        .expect("file will always have a parent directory")
+        .join(include);
+    if paths.contains(&needle) {
         return Some(needle);
     }
 
