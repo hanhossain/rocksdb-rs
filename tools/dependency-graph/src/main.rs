835a308cb2c69e3a257da363072c639efb7e936a
Han Hossain <hanhossain@outlook.com>
2024-02-11 21:33:55 -0800

    print all filepaths that we want to read

--- tools/dependency-graph/src/main.rs
+++ tools/dependency-graph/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use walkdir::WalkDir;
+
+fn main() -> anyhow::Result<()> {
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
+    let repo_root = std::fs::canonicalize(format!("{manifest_dir}/../.."))?;
+    let cxx_root = repo_root.join("rocksdb-cxx");
+
+    let paths = WalkDir::new(cxx_root)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| {
+            e.path()
+                .extension()
+                .map_or(false, |e| e == "cc" || e == "c" || e == "h")
+        })
+        .map(|e| e.into_path())
+        .collect::<Vec<_>>();
+
+    println!("{:?}", paths);
+
+    Ok(())
 }
