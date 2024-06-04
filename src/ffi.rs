4e86d237a2d5e7d14d0d7f5529365bf0effe8f90
Han Hossain <hanhossain@outlook.com>
2024-06-04 00:26:23 -0700

    consolidate options

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,19 +1,12 @@
 #![allow(unused_imports)]
 
 autocxx::include_cpp! {
+    #include "rocksdb/options.h"
     #include "rocksdb/slice.h"
+
     safety!(unsafe)
+
+    generate!("rocksdb::DbPath")
     generate!("rocksdb::Slice")
 }
 pub use ffi::*;
-
-pub mod options {
-    autocxx::include_cpp! {
-        #include "rocksdb/options.h"
-        safety!(unsafe)
-        name!(ffi_options)
-        exclude_utilities!()
-        generate!("rocksdb::DbPath")
-    }
-    pub use ffi_options::rocksdb::*;
-}
