5599293a92ef86713cc88533e9cc6babd980a809
Han Hossain <hanhossain@outlook.com>
2025-11-13 23:39:13 -0600

    manually import rocksdb::DbPath

--- src/ffi.rs
+++ src/ffi.rs
@@ -2,12 +2,10 @@
 #![allow(unsafe_op_in_unsafe_fn)]
 
 autocxx::include_cpp! {
-    #include "rocksdb/options.h"
     #include "rocksdb/slice.h"
 
     safety!(unsafe)
 
-    generate!("rocksdb::DbPath")
     generate!("rocksdb::Slice")
 }
 pub use ffi::*;
