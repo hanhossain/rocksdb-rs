c2537fc3f73a3fb6e1de35f334450642b34e8618
Han Hossain <hanhossain@outlook.com>
2025-11-16 19:17:03 -0600

    format

--- build_version.cc
+++ build_version.cc
@@ -21,70 +21,57 @@ static const std::string rocksdb_build_date = "rocksdb_build_date:2023-10-26 00:
 static const std::string rocksdb_build_date = "rocksdb_build_date:2023-10-26 00:00:00";
 #endif
 
-extern "C"
-{
+extern "C" {
 } // extern "C"
 
-std::unordered_map<std::string, rocksdb::RegistrarFunc> rocksdb::ObjectRegistry::builtins_ = {};
+std::unordered_map<std::string, rocksdb::RegistrarFunc> rocksdb::ObjectRegistry::builtins_ = {
+};
 
-namespace rocksdb
-{
-    static void AddProperty(std::unordered_map<std::string, std::string> *props, const std::string &name)
-    {
-        size_t colon = name.find(":");
-        if (colon != std::string::npos && colon > 0 && colon < name.length() - 1)
-        {
-            // If we found a "@:", then this property was a build-time substitution that failed.  Skip it
-            size_t at = name.find("@", colon);
-            if (at != colon + 1)
-            {
-                // Everything before the colon is the name, after is the value
-                (*props)[name.substr(0, colon)] = name.substr(colon + 1);
-            }
-        }
+namespace rocksdb {
+static void AddProperty(std::unordered_map<std::string, std::string> *props, const std::string& name) {
+  size_t colon = name.find(":");
+  if (colon != std::string::npos && colon > 0 && colon < name.length() - 1) {
+    // If we found a "@:", then this property was a build-time substitution that failed.  Skip it
+    size_t at = name.find("@", colon);
+    if (at != colon + 1) {
+      // Everything before the colon is the name, after is the value
+      (*props)[name.substr(0, colon)] = name.substr(colon + 1);
     }
+  }
+}
 
-    static std::unordered_map<std::string, std::string> *LoadPropertiesSet()
-    {
-        auto *properties = new std::unordered_map<std::string, std::string>();
-        AddProperty(properties, rocksdb_build_git_sha);
-        AddProperty(properties, rocksdb_build_git_tag);
-        AddProperty(properties, rocksdb_build_date);
-        return properties;
-    }
+static std::unordered_map<std::string, std::string>* LoadPropertiesSet() {
+  auto * properties = new std::unordered_map<std::string, std::string>();
+  AddProperty(properties, rocksdb_build_git_sha);
+  AddProperty(properties, rocksdb_build_git_tag);
+  AddProperty(properties, rocksdb_build_date);
+  return properties;
+}
 
-    const std::unordered_map<std::string, std::string> &GetRocksBuildProperties()
-    {
-        static std::unique_ptr<std::unordered_map<std::string, std::string>> props(LoadPropertiesSet());
-        return *props;
-    }
+const std::unordered_map<std::string, std::string>& GetRocksBuildProperties() {
+  static std::unique_ptr<std::unordered_map<std::string, std::string>> props(LoadPropertiesSet());
+  return *props;
+}
 
-    std::string GetRocksVersionAsString(bool with_patch)
-    {
-        std::string version = std::to_string(ROCKSDB_MAJOR) + "." + std::to_string(ROCKSDB_MINOR);
-        if (with_patch)
-        {
-            return version + "." + std::to_string(ROCKSDB_PATCH);
-        }
-        else
-        {
-            return version;
-        }
-    }
+std::string GetRocksVersionAsString(bool with_patch) {
+  std::string version = std::to_string(ROCKSDB_MAJOR) + "." + std::to_string(ROCKSDB_MINOR);
+  if (with_patch) {
+    return version + "." + std::to_string(ROCKSDB_PATCH);
+  } else {
+    return version;
+ }
+}
 
-    std::string GetRocksBuildInfoAsString(const std::string &program, bool verbose)
-    {
-        std::string info = program + " (RocksDB) " + GetRocksVersionAsString(true);
-        if (verbose)
-        {
-            for (const auto &it : GetRocksBuildProperties())
-            {
-                info.append("\n    ");
-                info.append(it.first);
-                info.append(": ");
-                info.append(it.second);
-            }
-        }
-        return info;
+std::string GetRocksBuildInfoAsString(const std::string& program, bool verbose) {
+  std::string info = program + " (RocksDB) " + GetRocksVersionAsString(true);
+  if (verbose) {
+    for (const auto& it : GetRocksBuildProperties()) {
+      info.append("\n    ");
+      info.append(it.first);
+      info.append(": ");
+      info.append(it.second);
     }
+  }
+  return info;
+}
 } // namespace rocksdb
